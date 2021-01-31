21989185affe915a6e59bf25b50614fe7d76495e
orangeztorm <taiwokenny45@gmail.com>
2021-01-31 17:39:37 +0100

    functional testing 2

--- budgetproject/functional_test/test_project_list_page.py
+++ budgetproject/functional_test/test_project_list_page.py
@@ -1,4 +1,6 @@
 import time
+from builtins import property
+
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from budget.models import Project
@@ -7,14 +9,44 @@ from django.urls import reverse
 
 class TestProjectListPage(StaticLiveServerTestCase):
 
-    def setup(self):
+    def setUp(self):
         self.browser = webdriver.Chrome('functional_test/chromedriver.exe')
 
     def tearDown(self):
-        self.browser = webdriver.Chrome('functional_test/chromedriver.exe')
         self.browser.close()
 
     def test_no_projects_alert_is_displayed(self):
-        self.browser = webdriver.Chrome('functional_test/chromedriver.exe')
         self.browser.get(self.live_server_url)
-        time.sleep(20)
+
+        # the user requests the page for the first time
+        alert = self.browser.find_element_by_class_name('noproject-wrapper')
+        self.assertEquals(alert.find_element_by_tag_name('h3').text, "Sorry, you don't have any projects, yet.")
+
+    def test_no_projects_alert_button_redirects_to_add_page(self):
+        # the user requests the page for the first time
+        add_url = self.live_server_url + reverse('add')
+        self.browser.find_element_by_tag_name('a').click()
+        self.assertEquals(self.browser.current_url, add_url)
+
+    def test_user_sees_project_list(self):
+        project1 = Project.objects.create(
+            name='project1',
+            budget=10000
+        )
+        self.browser.get(self.live_server_url)
+
+        # The user sees the project on the screen
+        self.assertEquals(self.browser.find_element_by_tag_name('h5').text, 'project1')
+
+    def test_user_is_redirected_to_project_detail(self):
+        project1 = Project.objects.create(
+            name='project1',
+            budget=10000
+        )
+        self.browser.get(self.live_server_url)
+
+        # The ser see the project on the screen , he then clicks and he's redirected
+        # tot he details page
+        detail_url = self.live_server_url + reverse('detail', args=[project1.slug])
+        self.browser.find_element_by_link_text('VISIT').click()
+        self.assertEquals(self.browser.current_url, detail_url)
