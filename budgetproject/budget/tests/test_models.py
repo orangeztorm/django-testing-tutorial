f5d133824cb40a8a20d553460a649576167e88e3
orangeztorm <taiwokenny45@gmail.com>
2021-01-31 16:15:07 +0100

    testing views

--- budgetproject/budget/tests/test_models.py
+++ budgetproject/budget/tests/test_models.py
@@ -1,11 +1,56 @@
 from django.test import TestCase
-from budget.models import Project,Category,Expense
+from budget.models import Project, Category, Expense
 
 
 class TestModels(TestCase):
 
     def setUp(self):
         self.project1 = Project.objects.create(
-            # name=
+            name=' Project 1',
+            budget=10000
         )
 
+    def test_project_is_assigned_slu_on_creation(self):
+        self.assertEquals(self.project1.slug, 'project-1')
+
+    def test_budget_left_property(self):
+        category1 = Category.objects.create(
+            project=self.project1,
+            name='development'
+        )
+        Expense.objects.create(
+            project=self.project1,
+            title='expense1',
+            amount=1000,
+            category=category1
+        )
+        Expense.objects.create(
+            project=self.project1,
+            title='expense2',
+            amount=2000,
+            category=category1
+        )
+        self.assertEquals(self.project1.budget_left, 7000)
+
+    def test_project_total_transaction(self):
+        project2 = Project.objects.create(
+            name='project2',
+            budget=20000
+        )
+        category1 = Category.objects.create(
+            project=self.project1,
+            name='development'
+        )
+        Expense.objects.create(
+            project=self.project1,
+            title='expense1',
+            amount=1000,
+            category=category1
+        )
+        Expense.objects.create(
+            project=project2,
+            title='expense2',
+            amount=2000,
+            category=category1
+        )
+        self.assertEquals(self.project1.total_transactions, 1)
